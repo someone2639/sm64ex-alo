0197b20d4077892168f2313f566c12102d5b7a18
scut <scut@gmail.com>
2020-12-24 17:39:30 -0500

    fixed HMC missing envfx

--- levels/hmc/script.c
+++ levels/hmc/script.c
@@ -16,6 +16,7 @@ const LevelScript level_hmc_entry[] = {
 INIT_LEVEL(),
 LOAD_MIO0(0x07, _hmc_segment_7SegmentRomStart, _hmc_segment_7SegmentRomEnd),
 LOAD_MIO0(0xA,_bits_skybox_mio0SegmentRomStart,_bits_skybox_mio0SegmentRomEnd),
+LOAD_MIO0(        /*seg*/ 0x0B, _effect_mio0SegmentRomStart, _effect_mio0SegmentRomEnd),
 LOAD_MIO0(8,_common0_mio0SegmentRomStart,_common0_mio0SegmentRomEnd),
 LOAD_RAW(15,_common0_geoSegmentRomStart,_common0_geoSegmentRomEnd),
 LOAD_MIO0(5,_group1_mio0SegmentRomStart,_group1_mio0SegmentRomEnd),
