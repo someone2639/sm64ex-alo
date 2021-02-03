4495b3c2e614d848d2515e10f16977f6b4717d6b
jesusyoshi54 <issa.j.cardona@mgmail.com>
2021-02-03 18:03:33 -0500

    noteblock merge

--- src/game/behaviors/NoteBlock.inc.c
+++ src/game/behaviors/NoteBlock.inc.c
@@ -1,17 +1,28 @@
 void bhv_noteblock_loop(void) {
+	f32 Yspd = 64.0f;
+	u32 CarmackMeme;
 	if (cur_obj_is_mario_on_platform()){
-		set_mario_action(&gMarioStates[0],ACT_DOUBLE_JUMP,0);
-		gMarioStates[0].vel[1]=(f32) o->oBehParams2ndByte*2;
+		//this is awful
+		if (gMarioStates[0].controller->buttonPressed & A_BUTTON){
+			Yspd = 200;
+		}
+		set_mario_action(gMarioState,ACT_DOUBLE_JUMP,0);
+		CarmackMeme = * (u32 *) &Yspd;
+		CarmackMeme+=(o->oBehParams2ndByte<<16);
+		Yspd = *(f32 *) &CarmackMeme;
+		gMarioStates[0].vel[1]=Yspd;
 		o->oAction=1;
+		gMarioStates[0].SelFallDmg=1;
 	}
 	if (o->oAction==1){
 		if (o->oTimer==4){
 			o->oAction=0;
+			o->oPosY = o->oHomeY;
 		}else{
 			if (o->oTimer>2){
-				o->oPosY += (f32)(o->oTimer%15)*6;
+				o->oPosY = o->oHomeY+(f32)(o->oTimer%3)*6;
 			}else{
-				o->oPosY -= (f32)(o->oTimer%15)*6;
+				o->oPosY = o->oHomeY-(f32)(o->oTimer)*6;
 			}
 		}
 	}
